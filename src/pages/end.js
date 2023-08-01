3c279afe8f8ec2575d7440fa174fa64fd16aed27
BeedinM <douglas.bedin@gmail.com>
2023-08-01 10:25:27 -0300

    adicionando página de 'game over'

---
+++ src/pages/end.js
@@ -0,0 +1,8 @@
+export default function End() {
+    return (
+      <div>
+        <h1>Fim do jogo</h1>
+        <p>Você escapou da masmorra com sucesso!</p>
+      </div>
+    );
+  }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
+import Link from "next/link"
+
 export default function Inicio() {
     return (
-        <>
-        
-        </>
+        <div>
+            <h1>Bem-vindo</h1>
+            <Link href='/game'>Iniciar o Ogoj</Link>
+        </div>
     )
 }
