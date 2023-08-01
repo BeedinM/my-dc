1b01945b1e153f90b6a51ae32a9aaf5dacb9b67e
BeedinM <douglas.bedin@gmail.com>
2023-08-01 16:04:53 -0300

    arrumando geração de mapas. Corredores ligados necessariamente à salas e não ocasionar corredores diagonais que o personagem não passa

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../../styles/game.module.css';
 
 const DUNGEON_SIZE = 30;
@@ -24,22 +24,28 @@ const createRoom = (dungeon, x, y, width, height) => {
   }
 };
 
-const createCorridor = (dungeon, x1, y1, x2, y2) => {
-  let x = x1;
-  let y = y1;
-  while (x !== x2 || y !== y2) {
-    if (x !== x2) {
-      x += x < x2 ? 1 : -1;
-    }
-    if (y !== y2) {
-      y += y < y2 ? 1 : -1;
-    }
-    dungeon[y][x] = '.';
-  }
+const getRandomRoom = (rooms) => {
+  return rooms[getRandomInt(0, rooms.length - 1)];
 };
+
+const createCorridor = (dungeon, x1, y1, x2, y2) => {
+    let x = x1;
+    let y = y1;
+  
+    while (x !== x2) {
+      x += x < x2 ? 1 : -1;
+      dungeon[y][x] = '.';
+    }
+  
+    while (y !== y2) {
+      y += y < y2 ? 1 : -1;
+      dungeon[y][x] = '.';
+    }
+  };
 
 const generateRandomDungeon = () => {
   const dungeon = createEmptyDungeon();
+  const rooms = [];
 
   // Gerar salas
   for (let i = 0; i < 5; i++) {
@@ -48,15 +54,16 @@ const generateRandomDungeon = () => {
     const roomX = getRandomInt(1, DUNGEON_SIZE - roomWidth - 1);
     const roomY = getRandomInt(1, DUNGEON_SIZE - roomHeight - 1);
     createRoom(dungeon, roomX, roomY, roomWidth, roomHeight);
+    rooms.push({ x: roomX, y: roomY, width: roomWidth, height: roomHeight });
   }
 
   // Gerar corredores
-  const rooms = [{ x: 0, y: 0 }];
-  for (let i = 1; i < 5; i++) {
-    const prevRoom = rooms[i - 1];
-    const room = { x: getRandomInt(1, DUNGEON_SIZE - 1), y: getRandomInt(1, DUNGEON_SIZE - 1) };
-    rooms.push(room);
-    createCorridor(dungeon, prevRoom.x, prevRoom.y, room.x, room.y);
+  const connectedRooms = [rooms[0]];
+  for (let i = 1; i < rooms.length; i++) {
+    const prevRoom = getRandomRoom(connectedRooms);
+    const room = rooms[i];
+    createCorridor(dungeon, prevRoom.x + Math.floor(prevRoom.width / 2), prevRoom.y + Math.floor(prevRoom.height / 2), room.x + Math.floor(room.width / 2), room.y + Math.floor(room.height / 2));
+    connectedRooms.push(room);
   }
 
   return dungeon;
@@ -65,78 +72,76 @@ const generateRandomDungeon = () => {
 export default function Game() {
   const [dungeon, setDungeon] = useState([]);
   const [playerPosition, setPlayerPosition] = useState({ x: 15, y: 15 });
-  const [gameOver, setGameOver] = useState(false);
-
-  const initializeGame = () => {
-    const newDungeon = generateRandomDungeon();
-    // Posicionar o jogador (P) em uma posição válida que não seja parede ('#') ou saída ('E')
-    let playerX, playerY;
-    do {
-      playerX = getRandomInt(0, DUNGEON_SIZE - 1);
-      playerY = getRandomInt(0, DUNGEON_SIZE - 1);
-    } while (
-      newDungeon[playerY][playerX] === '#' ||
-      newDungeon[playerY][playerX] === 'E' ||
-      newDungeon[playerY][playerX] === 'P'
-    ); // Evitar que o jogador nasça em posições ocupadas por parede, saída ou outro jogador
-    newDungeon[playerY][playerX] = 'P'; // Posicionar o jogador na masmorra
-    setDungeon(newDungeon);
-    setPlayerPosition({ x: playerX, y: playerY });
-  };
 
   useEffect(() => {
+    const initializeGame = () => {
+      const newDungeon = generateRandomDungeon();
+      // Posicionar o jogador (P) em uma posição válida que não seja parede ('#') ou saída ('E')
+      let playerX, playerY;
+      do {
+        playerX = getRandomInt(0, DUNGEON_SIZE - 1);
+        playerY = getRandomInt(0, DUNGEON_SIZE - 1);
+      } while (
+        newDungeon[playerY][playerX] === '#' ||
+        newDungeon[playerY][playerX] === 'E' ||
+        newDungeon[playerY][playerX] === 'P'
+      ); // Evitar que o jogador nasça em posições ocupadas por parede, saída ou outro jogador
+      newDungeon[playerY][playerX] = 'P'; // Posicionar o jogador na masmorra
+      setDungeon(newDungeon);
+      setPlayerPosition({ x: playerX, y: playerY });
+    };
+
     initializeGame();
   }, []);
 
-  const handleKeyPress = (event) => {
-    if (gameOver) return; // Impedir que o jogador se mova após a derrota
-
-    const { key } = event;
-    let newPosition = { ...playerPosition };
-
-    if (key === 'ArrowUp') {
-      newPosition.y = Math.max(newPosition.y - 1, 0);
-    } else if (key === 'ArrowDown') {
-      newPosition.y = Math.min(newPosition.y + 1, DUNGEON_SIZE - 1);
-    } else if (key === 'ArrowLeft') {
-      newPosition.x = Math.max(newPosition.x - 1, 0);
-    } else if (key === 'ArrowRight') {
-      newPosition.x = Math.min(newPosition.x + 1, DUNGEON_SIZE - 1);
-    }
-
-    // Verificar se a posição é válida antes de atualizar o estado
-    if (
-      newPosition.y >= 0 &&
-      newPosition.y < DUNGEON_SIZE &&
-      newPosition.x >= 0 &&
-      newPosition.x < DUNGEON_SIZE &&
-      dungeon[newPosition.y][newPosition.x] !== '#'
-    ) {
-      // Verificar encontro com inimigos ('E')
-      if (dungeon[newPosition.y][newPosition.x] === 'E') {
-        // Derrota: o jogador encontrou um inimigo
-        setGameOver(true);
-        alert('Você foi derrotado! O inimigo te pegou!');
-      } else {
-        // Atualizar a nova posição do jogador na masmorra
-        const updatedDungeon = [...dungeon];
-        updatedDungeon[playerPosition.y][playerPosition.x] = '.'; // Limpar a posição anterior do jogador
-        updatedDungeon[newPosition.y][newPosition.x] = 'P';
-        setDungeon(updatedDungeon);
-        // Atualizar a posição do jogador no estado
-        setPlayerPosition(newPosition);
-      }
-    }
-  };
-
   useEffect(() => {
-    if (!gameOver) {
-      window.addEventListener('keydown', handleKeyPress);
-      return () => {
-        window.removeEventListener('keydown', handleKeyPress);
-      };
-    }
-  }, [dungeon, playerPosition, gameOver]);
+    const handleKeyPress = (event) => {
+      const { key } = event;
+      let newPosition = { ...playerPosition };
+
+      if (key === 'ArrowUp') {
+        newPosition.y = Math.max(newPosition.y - 1, 0);
+      } else if (key === 'ArrowDown') {
+        newPosition.y = Math.min(newPosition.y + 1, DUNGEON_SIZE - 1);
+      } else if (key === 'ArrowLeft') {
+        newPosition.x = Math.max(newPosition.x - 1, 0);
+      } else if (key === 'ArrowRight') {
+        newPosition.x = Math.min(newPosition.x + 1, DUNGEON_SIZE - 1);
+      }
+
+      // Verificar se a posição é válida antes de atualizar o estado
+      if (
+        newPosition.y >= 0 &&
+        newPosition.y < DUNGEON_SIZE &&
+        newPosition.x >= 0 &&
+        newPosition.x < DUNGEON_SIZE &&
+        dungeon[newPosition.y][newPosition.x] !== '#'
+      ) {
+        // Verificar encontro com inimigos ('E')
+        if (dungeon[newPosition.y][newPosition.x] === 'E') {
+          // Derrota: o jogador encontrou um inimigo
+          alert('Você foi derrotado! O inimigo te pegou!');
+          // Reiniciar o jogo
+          setDungeon(generateRandomDungeon());
+          setPlayerPosition({ x: 0, y: 0 });
+        } else {
+          // Limpar a posição anterior do jogador
+          const updatedDungeon = [...dungeon];
+          updatedDungeon[playerPosition.y][playerPosition.x] = '.';
+          // Atualizar a posição do jogador no estado
+          setPlayerPosition(newPosition);
+          // Atualizar a nova posição do jogador na masmorra
+          updatedDungeon[newPosition.y][newPosition.x] = 'P';
+          setDungeon(updatedDungeon);
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [dungeon, playerPosition]);
 
   return (
     <div className={styles.gameContainer}>
@@ -152,4 +157,4 @@ export default function Game() {
       ))}
     </div>
   );
-}
+};
