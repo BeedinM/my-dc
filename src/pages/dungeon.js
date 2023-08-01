2890c32a79cbcccee880ee4ada4b76e841106e42
BeedinM <douglas.bedin@gmail.com>
2023-08-01 14:19:24 -0300

    add procedural map e colisão com paredes

--- src/pages/dungeon.js
+++
@@ -1,6 +0,0 @@
-export default function Dungeon() {
-    return (
-        <>
-        </>
-    )
-}
--- src/pages/game.js
+++ src/pages/game.js
@@ -1,50 +1,64 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../../styles/game.module.css';
 
-const dungeonWidth = 20;
-const dungeonHeight = 10;
+const DUNGEON_SIZE = 20;
 
-const initialDungeon = [
-    '####################',
-    '#..................#',
-    '#..................#',
-    '#....#.............#',
-    '#....#.............#',
-    '#....#.............#',
-    '#..................#',
-    '#..................#',
-    '####################'
-];
+const generateRandomDungeon = () => {
+  const dungeon = [];
+  for (let i = 0; i < DUNGEON_SIZE; i++) {
+    const row = [];
+    for (let j = 0; j < DUNGEON_SIZE; j++) {
+      // Usar '#' para paredes e '.' para pisos.
+      row.push(Math.random() < 0.3 ? '#' : '.');
+    }
+    dungeon.push(row);
+  }
+  return dungeon;
+};
 
 export default function Game() {
-  const [ dungeon, setDungeon ] = useState(initialDungeon);
-  const [ playerPosition, setPlayerPosition ] = useState({ x: 1, y: 1 })
+    const [dungeon, setDungeon] = useState([]);
+    const [playerPosition, setPlayerPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    setDungeon(generateRandomDungeon());
+  }, []);
 
   const handleKeyPress = (event) => {
     const { key } = event;
-    const { x, y } = playerPosition;
-
-    if (key === 'ArrowUp' && dungeon[y - 1][x] === '.') {
-        setPlayerPosition({ x, y: y - 1});
-    } else if (key === 'ArrowDown' && dungeon[y + 1][x] === '.') {
-        setPlayerPosition({x, y: y + 1});
-    } else if (key === 'ArrowLeft' && dungeon[y][x - 1] === '.') {
-        setPlayerPosition({ x: x - 1, y});
-    } else if (key === 'ArrowRight' && dungeon[y][x + 1] === '.') {
-        setPlayerPosition({x: x + 1, y});
+    let newPosition = { ...playerPosition };
+  
+    if (key === 'ArrowUp') {
+      newPosition.y = Math.max(newPosition.y - 1, 0);
+    } else if (key === 'ArrowDown') {
+      newPosition.y = Math.min(newPosition.y + 1, DUNGEON_SIZE - 1);
+    } else if (key === 'ArrowLeft') {
+      newPosition.x = Math.max(newPosition.x - 1, 0);
+    } else if (key === 'ArrowRight') {
+      newPosition.x = Math.min(newPosition.x + 1, DUNGEON_SIZE - 1);
     }
-  }
+  
+    // Verificar colisão com as paredes ('#') antes de atualizar a posição
+    if (dungeon[newPosition.y][newPosition.x] !== '#') {
+      setPlayerPosition(newPosition);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [playerPosition]);
 
   return (
-    <div className={styles.gameContainer} tabIndex={0} onKeyDown={handleKeyPress}>
+    <div className={styles.gameContainer}>
       <h1>Calabouço Crawler</h1>
-      {dungeon.map((row, y) => (
-        <div key={y} className={styles.row}>
-            {row.split('').map((cell, x) => (
-                <span key={x} className={styles.cell}>
-                    {playerPosition.x === x && playerPosition.y === y ? 'P' : cell}
-                </span>
-            ))}
+      {dungeon.map((row, rowIndex) => (
+        <div key={rowIndex} className={styles.row}>
+          {row.map((cell, columnIndex) => (
+            <span key={columnIndex} className={styles.cell}>{playerPosition.x === columnIndex && playerPosition.y === rowIndex ? 'P' : cell}</span>
+          ))}
         </div>
       ))}
     </div>
